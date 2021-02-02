9180110ec26a337a28aa46ff5bf0c8e8bb88e48c
TamojitSaha <sahatamo1995@gmail.com>
2021-02-03 01:57:34 +0530

    Updated test1, added test 3

--- src/test1/main.cpp
+++ src/test1/main.cpp
@@ -3,8 +3,8 @@
 TaskHandle_t Task1, Task2;
 int count1 = 0, lastCount1 = 0, count2 = 0, lastCount2 = 0;
 
-#define BTN_1 GPIO_NUM_15
-#define BTN_2 GPIO_NUM_17
+#define BTN_1 GPIO_NUM_18
+#define BTN_2 GPIO_NUM_19
 
 #define LED_1 GPIO_NUM_25
 #define LED_2 GPIO_NUM_26
@@ -39,7 +39,7 @@ typedef enum
 //usage: digWrite(GPIO_NUM_25, OFF);
 void digWrite(gpio_num_t gpio, pin_logic logic)
 {
-  gpio_set_direction(gpio, GPIO_MODE_OUTPUT);
+
   gpio_set_level(gpio, (uint32_t)logic);
 }
 
@@ -55,10 +55,12 @@ void IRAM_ATTR count2_isr(void *args)
 
 void blink_times(gpio_num_t led, byte times)
 {
+
   if (times > 0)
   {
     for (int i = 0; i < times; i++)
     {
+
       digWrite(led, ON);
       delay(500);
       digWrite(led, OFF);
@@ -69,24 +71,28 @@ void blink_times(gpio_num_t led, byte times)
 
 void _task1func(void *params)
 {
-  for (;;)
+  String taskMessage = "Task 1 running on core ";
+  taskMessage = taskMessage + xPortGetCoreID();
+  // int cpu_id = esp_intr_get_cpu((intr_handle_t)count2_isr);
+
+  // taskMessage += "\n Count2 ISR running on Core id:" + cpu_id;
+
+  while (true)
   {
-    esp_err_t allocationStatus = esp_intr_alloc(ETS_GPIO_INTR_SOURCE, 0, count1_isr, NULL, NULL);
-    // uxTaskGetStackHighWaterMark
-    int cpu_id = esp_intr_get_cpu((intr_handle_t)count1_isr);
-
-    Serial.print("Allocation Result: ");
-    Serial.println(esp_err_to_name(allocationStatus));
-    Serial.print("count1 ISR running on Core id: ");
-    Serial.println(cpu_id);
-
     if (count1 == 10)
       count1 = 0;
     if (count1 != lastCount1)
     {
-      blink_times(LED_1, (byte)count1);
+      Serial.println(taskMessage);
+      Serial.println("\nInterrupt in Task 1");
+      UBaseType_t uxHighWaterMark;
+      uxHighWaterMark = uxTaskGetStackHighWaterMark(Task1);
+      Serial.print("Task 1 Stack Size: ");
+      Serial.println(uxHighWaterMark);
+      // blink_times(LED_1, (byte)count1);
       lastCount1 = count1;
     }
+    delay(10);
   }
 
   //gpio_uninstall_isr_service
@@ -95,23 +101,26 @@ void _task1func(void *params)
 
 void _task2func(void *params)
 {
+  String taskMessage = "Task 2 running on core ";
+  taskMessage = taskMessage + xPortGetCoreID();
+
   for (;;)
   {
-    esp_err_t allocationStatus = esp_intr_alloc(ETS_GPIO_INTR_SOURCE, 0, count2_isr, NULL, NULL);
-    int cpu_id = esp_intr_get_cpu((intr_handle_t)count2_isr);
-
-    Serial.print("Allocation Result: ");
-    Serial.println(esp_err_to_name(allocationStatus));
-    Serial.print("count2 ISR running on Core id: ");
-    Serial.println(cpu_id);
 
     if (count2 == 10)
       count2 = 0;
     if (count2 != lastCount2)
     {
-      blink_times(LED_2, (byte)count2);
+      Serial.println(taskMessage);
+      Serial.println("\nInterrupt in Task 2");
+      UBaseType_t uxHighWaterMark;
+      uxHighWaterMark = uxTaskGetStackHighWaterMark(Task2);
+      Serial.print("Task 2 Stack Size: ");
+      Serial.println(uxHighWaterMark);
+      //blink_times(LED_2, (byte)count2);
       lastCount2 = count2;
     }
+    delay(10);
   }
 }
 
@@ -136,11 +145,12 @@ void btn2_init(void)
 {
 
   button2.init(BTN_2, GPIO_MODE_INPUT, GPIO_PULLUP_ONLY, GPIO_INTR_NEGEDGE);
-  gpio_isr_handler_add(BTN_2, count1_isr, NULL);
+  gpio_isr_handler_add(BTN_2, count2_isr, NULL);
 }
 
 void setup()
 {
+  Serial.begin(115200);
   //Install the driver’s GPIO ISR handler service, which allows per-pin GPIO interrupt handlers.
   gpio_install_isr_service(0);
   btn1_init();
@@ -148,12 +158,11 @@ void setup()
   // led1_init();
   // led2_init();
 
-  Serial.begin(115200);
-
+  delay(1000);
   xTaskCreatePinnedToCore(
       _task1func,
       "increment count1",
-      1024,
+      10000,
       NULL,
       1,
       &Task1,
@@ -162,23 +171,20 @@ void setup()
   xTaskCreatePinnedToCore(
       _task2func,
       "increment count2",
-      1024, //stack depth
-      NULL, //passing params to task
-      1,    //Priority
+      10000, //stack depth
+      NULL,  //passing params to task
+      1,     //Priority
       &Task2,
       1 //Core ID
   );
-  UBaseType_t uxHighWaterMark;
-  uxHighWaterMark = uxTaskGetStackHighWaterMark(Task1);
-  Serial.print("Task 1 Stack Size: ");
-  Serial.println(uxHighWaterMark);
-  uxHighWaterMark = uxTaskGetStackHighWaterMark(Task2);
-  Serial.print("Task 2 Stack Size: ");
-  Serial.println(uxHighWaterMark);
+  delay(500);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  delay(500);
+  Serial.println("Starting main loop...");
+  while (true)
+  {
+  }
 }
